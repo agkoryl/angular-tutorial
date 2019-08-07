8c604ee5c2a371925939354dc8f56f919d63c29e
Agnieszka Koryl <ag.koryl@gmail.com>
2019-08-07 17:19:12 +0200

    angular tutorial end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -19,7 +21,9 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -7,13 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  h1styles: boolean = false;
+  greenStyle: boolean = false;
+
+  users: Object;
+
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.data.getUsers().subscribe(data => {
+      this.users = data;
+      console.log(this.users);
+    });
+    
   }
 
   firstClick() {
-    console.log("BLE BLE")
+    let input = document.getElementById("input");
+    input.innerText = "clicked";
+    
+    if(this.h1styles === true) {
+      this.h1styles = false;
+    } else {
+      this.h1styles = true;
+    }
+  }
+
+  styleVariableClick() {
+    
+    if(this.greenStyle === true) {
+      this.greenStyle = false;
+    } else {
+      this.greenStyle = true;
+    }
+  }
+
+  importedClick() {
+    this.data.clickToExport();
   }
 
 }
